cb91442c6c701da533e570b93b3eb96eae85d546
imransamad <46784351+imransamad@users.noreply.github.com>
2019-01-17 21:27:10 +0800

    1

---
+++ index.php
@@ -0,0 +1,8 @@
+<table width="337">
+
+<td width="298" height="300" align="center"> <p><strong> NAMA : imran samad </strong> </p>
+	<p><strong> NIM : 215280002 </strong></p>
+	<p><strong> PHONE : 082333319459 </strong></p>
+	<p><strong> EMAIL : imransamad21@gmail.com</strong></p>
+	<p><strong> South Sulawesi </strong></p> 
+	</table>
